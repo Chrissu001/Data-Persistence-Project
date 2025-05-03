70b6d98f22f1e4979f7da82d1d74b06f2682d3fd
Chrissu001 <chrisnelsondeslate@gmail.com>
2025-05-03 18:58:49 +0800

    added a high score text and reset button in the title screen
    - Added a reset high score button
    - Added a high score in the title screen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,18 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public Text playerNameText;
 
-     void Start()
+    void Start()
     {
         int highScore = PlayerPrefs.GetInt("HighScore", 0);
-        string highScorePlayer = PlayerPrefs.GetString("HighScoreName","Unknown Player");
+        string highScorePlayer = PlayerPrefs.GetString("HighScoreName", "Unknown Player");
         playerNameText.text = $"Best Score: {highScorePlayer} {highScore}";
     }
 
+    public void ResetHighScore()
+    {
+        PlayerPrefs.DeleteKey("HighScore");
+        PlayerPrefs.DeleteKey("HighScoreName");
+        PlayerPrefs.Save();
 
+        playerNameText.text = "Best Score: No One";
+    }
 }
 
--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 public class MenuManager : MonoBehaviour
 {
     public InputField nameInput;
-
+   
     public void StartGame()
     {
         PlayerData.playerName = nameInput.text; // Save the name
